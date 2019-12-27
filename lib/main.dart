be845f837be1047bb89c5f4fe7777a256b352aa4
jessecodesNblends <ocran.jss@gmail.com>
2019-12-27 12:09:36 +0000

    Add vertical list item

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,43 @@ class HomePage extends StatelessWidget{
 
 	@override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+		appBar: AppBar(
+			title: Text("Ice On Ace"),
+		),
+		body: Column(
+			children: <Widget>[
+				Card(
+					child: Row(
+						children: <Widget>[
+							Container(
+								margin: EdgeInsets.all(8),
+								padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+								decoration: BoxDecoration(
+									border: Border.all(width: 2, color: LIGHTLIME), 
+									borderRadius: BorderRadius.all(Radius.circular(8))),
+								child: Text("IMG"),
+							),
+							Container(
+							  child: Row(
+							  	mainAxisAlignment: MainAxisAlignment.spaceBetween,
+							  	children: <Widget>[
+							  		Column(
+							  			crossAxisAlignment: CrossAxisAlignment.start,
+							  			children: <Widget>[
+							  				Text("Smoothie"),
+							  				Text("Mango"),
+							  				Text("Price", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+							  			],
+							  		),
+							  	],
+							  ),
+							),
+						],
+					),
+				)
+			],
+		),
+    );
   }
 }
