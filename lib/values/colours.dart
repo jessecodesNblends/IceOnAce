500a5fb61e967f874af57caa3c5bb6389b253e0f
jessecodesNblends <ocran.jss@gmail.com>
2019-12-27 17:52:32 +0000

    Add colours

--- lib/values/colours.dart
+++ lib/values/colours.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
 class IceOnAceColours {
-    static const LIGHTLIME = Color.fromRGBO(87, 95, 58, 1);
-    static const LIME = Color.fromRGBO(76, 85, 42, 1);
-    static const GREEN = Color.fromRGBO(33, 65, 53, 1);
+    
 }
