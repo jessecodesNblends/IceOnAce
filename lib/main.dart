bca54d31a26c7ef7d58f7961d7f6dcedb7a7e370
jessecodesNblends <ocran.jss@gmail.com>
2019-12-27 11:14:45 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main () {
+	runApp(
+		MaterialApp(
+			title: "IceOnAce",
+			home: HomePage(),
+		)
+	);
+}
+
+class HomePage extends StatelessWidget{
+	
+	@override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
