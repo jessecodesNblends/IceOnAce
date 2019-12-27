483cd9076f1af646a4acbf467652d7f35ef7c72d
jessecodesNblends <ocran.jss@gmail.com>
2019-12-27 11:15:32 +0000

    Add app colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ void main () {
 }
 
 class HomePage extends StatelessWidget{
-	
+	static const LIGHTLIME = Color.fromRGBO(87, 95, 58, 1);
+	static const LIME = Color.fromRGBO(76, 85, 42, 1);
+	static const GREEN = Color.fromRGBO(33, 65, 53, 1);
+
 	@override
   Widget build(BuildContext context) {
     return null;
