95b4ad9f77b746c2b39a547a7f92cbbe18ea4b17
jessecodesNblends <ocran.jss@gmail.com>
2019-12-27 13:26:08 +0000

    Remove product list item

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-
-// Model imports
 import 'package:ice_on_ace/models/product.dart';
-
-// IceOnAce imports
-import './widgets/product_item_widget.dart';
 
 void main () {
 	runApp(
@@ -34,7 +29,35 @@ class HomePage extends StatelessWidget{
 			body: ListView.builder(
 				itemCount: products.length,
 				itemBuilder: (context, index) {
-					return ProductItem();
+					return Card(
+						child: Row(
+							children: <Widget>[
+								Container(
+									margin: EdgeInsets.all(8),
+									padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+									decoration: BoxDecoration(
+										border: Border.all(width: 2, color: LIGHTLIME), 
+										borderRadius: BorderRadius.all(Radius.circular(8))),
+									child: Text("IMG"),
+								),
+								Container(
+									child: Row(
+										mainAxisAlignment: MainAxisAlignment.spaceBetween,
+										children: <Widget>[
+											Column(
+												crossAxisAlignment: CrossAxisAlignment.start,
+												children: <Widget>[
+													Text(products[index].type),
+													Text(products[index].title),
+													Text("${products[index].price.toStringAsFixed(2)}GH", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+												],
+											),
+										],
+									),
+								),
+							],
+						),
+					);
 				}
 			),
 		);
