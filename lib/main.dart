40683ae6e2598bfb14ad55d87bc89c2a7559e2b2
jessecodesNblends <ocran.jss@gmail.com>
2019-12-28 05:54:34 +0000

    Create home widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ void main () {
 }
 
 class HomePage extends StatelessWidget{
-	static const LIGHTLIME = Color.fromRGBO(87, 95, 58, 1);
-	static const LIME = Color.fromRGBO(76, 85, 42, 1);
-	static const GREEN = Color.fromRGBO(33, 65, 53, 1);
 
 	final products = [
 		Product(title: "Mango", price: 2.50, type: "Smoothie"),
