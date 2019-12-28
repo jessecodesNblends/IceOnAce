cc03e027d90b9ce62279b6a34edb5aba14a8e2a2
jessecodesNblends <ocran.jss@gmail.com>
2019-12-28 06:20:28 +0000

    Split all products widget

--- lib/main.dart
+++ lib/main.dart
@@ -54,50 +54,7 @@ class HomePage extends StatelessWidget{
 			  			),
 			    		
 
-			    		Text("All Products",
-			    			style: TextStyle(fontSize: 16, color: IceOnAceColours.GREEN),
-			    		),
-			    		Container(
-			    			margin: EdgeInsets.fromLTRB(0, 4, 0, 0),
-			    			height: 250,
-			    			child: ListView.builder(
-			    				itemCount: products.length,
-			    				itemBuilder: (context, index) {
-			    					return 
-			    					// Card(
-			    					// 	child: 
-			    						Row(
-			    							children: <Widget>[
-			    								Container(
-			    									margin: EdgeInsets.fromLTRB(0, 8, 8, 8),
-			    									padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
-			    									decoration: BoxDecoration(
-			    										border: Border.all(width: 2, color: IceOnAceColours.LIGHTLIME), 
-			    										borderRadius: BorderRadius.all(Radius.circular(8))
-													),
-			    									child: Text("IMG"),
-			    								),
-			    								Container(
-			    									child: Row(
-			    										mainAxisAlignment: MainAxisAlignment.spaceBetween,
-			    										children: <Widget>[
-			    											Column(
-			    												crossAxisAlignment: CrossAxisAlignment.start,
-			    												children: <Widget>[
-			    													Text(products[index].type, style: TextStyle(color: IceOnAceColours.GREEN),),
-			    													Text(products[index].title, style: TextStyle(color: IceOnAceColours.GREEN),),
-			    													Text("${products[index].price.toStringAsFixed(2)}GH", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: IceOnAceColours.GREEN),)
-			    												],
-			    											),
-			    										],
-			    									),
-			    								),
-			    							],
-			    						);
-			    					// );
-			    				}
-			    			),
-			    		),
+			    		
 			    	],
 			    ),
 			  ),
---
+++ lib/widgets/all_products_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+// IceOnAce imports
+import '../values/colours.dart';
+import '../models/product.dart';
+
+class AllProducts extends StatelessWidget {
+	final List<Product> products;
+
+	AllProducts(this.products);
+
+	@override
+	Widget build(BuildContext context) {
+		return Column(
+			children: <Widget>[
+				Text("All Products",
+			    			style: TextStyle(fontSize: 16, color: IceOnAceColours.GREEN),
+			    		),
+			    		Container(
+			    			margin: EdgeInsets.fromLTRB(0, 4, 0, 0),
+			    			height: 250,
+			    			child: ListView.builder(
+			    				itemCount: products.length,
+			    				itemBuilder: (context, index) {
+			    					return 
+			    					// Card(
+			    					// 	child: 
+			    						Row(
+			    							children: <Widget>[
+			    								Container(
+			    									margin: EdgeInsets.fromLTRB(0, 8, 8, 8),
+			    									padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+			    									decoration: BoxDecoration(
+			    										border: Border.all(width: 2, color: IceOnAceColours.LIGHTLIME), 
+			    										borderRadius: BorderRadius.all(Radius.circular(8))
+													),
+			    									child: Text("IMG"),
+			    								),
+			    								Container(
+			    									child: Row(
+			    										mainAxisAlignment: MainAxisAlignment.spaceBetween,
+			    										children: <Widget>[
+			    											Column(
+			    												crossAxisAlignment: CrossAxisAlignment.start,
+			    												children: <Widget>[
+			    													Text(products[index].type, style: TextStyle(color: IceOnAceColours.GREEN),),
+			    													Text(products[index].title, style: TextStyle(color: IceOnAceColours.GREEN),),
+			    													Text("${products[index].price.toStringAsFixed(2)}GH", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: IceOnAceColours.GREEN),)
+			    												],
+			    											),
+			    										],
+			    									),
+			    								),
+			    							],
+			    						);
+			    					// );
+			    				}
+			    			),
+			    		),
+			],
+		);
+	}
+}
